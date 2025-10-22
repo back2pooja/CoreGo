8d18f7aebb82fd0f2f3bc3bbc4164aed08ab02a6
Pooja Jaiswar <pooja.jaiswar.987@gmail.com>
2025-10-22 15:59:29 +0530

    CoreGo: Sequence flow in golang

--- core.go
+++ core.go
@@ -6,6 +6,15 @@ import (
 )
 
 func main() {
-	fmt.Printf("go core started....")
-	fmt.Printf("go version" + runtime.Version())
+	for i := 0; i < 10; i++ {
+		fmt.Println("go core started....")
+		fmt.Println("go version: " + runtime.Version())
+
+	}
+	fmt.Println("This is the first statement to run")
+	fmt.Println("This is the second statement to run")
+	x := 40
+	y := 5
+	fmt.Printf("x=%v, y=%v\n", x, y)
+
 }
