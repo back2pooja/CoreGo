917ebf3d8fae560f2b4d34850a17598d737690a4
Pooja Jaiswar <pooja.jaiswar.987@gmail.com>
2025-10-23 15:19:35 +0530

    CoreGo: Operators in golang.

--- core.go
+++ core.go
@@ -13,13 +13,17 @@ func main() {
 	}
 	fmt.Println("This is the first statement to run")
 	fmt.Println("This is the second statement to run")
-	x := 40
-	y := 5
+	x := 42
+	y := 42
 	fmt.Printf("x=%v, y=%v\n", x, y)
 	if x < 42 {
 		fmt.Println("Less than the meaning of life")
 	}
+	/*
+	   "if " statement specify the conditional execution of two branches according tp the value of a boolean expression. if expression evaluates to true
+	   , the "if" branch is executed , otherwise if present , the "else" branch is executed/
 
+	*/
 	if x < 42 {
 		fmt.Println("Less than the meaning of life")
 	} else {
@@ -31,6 +35,22 @@ func main() {
 		fmt.Println("equals to the meaning of life")
 	} else {
 		fmt.Println("greater than the meaning of life")
+	}
+	/*
+		    Logical operators apply to boolean values
+		     and yield a result of thr same type as the operands.
+		the right oprand is evaluated conditionally
+	*/
+
+	if x < 42 && y < 42 {
+		fmt.Println("both are less than the meaning of life")
+	}
+
+	if x > 30 || x < 42 {
+		fmt.Println("x is greater than the meaning of life")
+	}
+	if x != 42 && y != 10 {
+		fmt.Println("x is not 42 and y is not 10")
 	}
 
 }
