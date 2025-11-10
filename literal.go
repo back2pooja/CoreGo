58b510a6ef58b18292423c03e108d0cc5b98c294
Pooja Jaiswar <pooja.jaiswar.987@gmail.com>
2025-11-10 15:42:14 +0530

    CoreGo: For Range Statement

--- literal.go
+++ literal.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	/*	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+
+		for i, v := range x {
+			fmt.Printf("%v  /t %T \t %v\n", v, v, i)
+		}
+
+		fmt.Printf("%T \t %#v\n", x, x)
+		fmt.Printf(" %T \t %v\n", x, x)*/
+
 	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
 
-	for i, v := range x {
-		fmt.Printf("%v  /t %T \t %v\n", v, v, i)
-	}
+	fmt.Println(x[:5])
+	fmt.Println(x[5:])
+	fmt.Println(x[2:7])
+	fmt.Println(x[1:6])
 
 }
